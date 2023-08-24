b01166a8622ede6cdc03a282049fa759bcb59761
richie nolan <richienolan23@gmail.com>
2023-08-24 20:53:55 +0000

    add function to create the game boards for the player and computer

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 credits:
 
-https://www.istockphoto.com/vector/warship-and-submarine-side-view-in-the-sea-gm610230744-104708245?phrase=battleship+background
+https://stock.adobe.com/ie/images/battleship-board-game-sea-battle-toy-ships-on-a-plastic-board-perfect-hit-victory-or-defeat-blue-background-blur/346456968
+battleship background for body
+
+https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/draggable
+site for draggable feature in html
+
+https://stackoverflow.com/questions/12622465/creating-a-div-element-inside-a-div-element-in-javascript
+create a div with JS inside a div
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
+const gameArea = document.querySelector('#game-area')
 const shipsContainer = document.querySelector('#ships-container')
 const flipButton = document.querySelector('#flip-button')
-let angle = 0 
 
+
+
+// Function which runs a if else statement which targets the children of the 'ships-container', 
+// when the flipButton event listener is clicked it calls the flipFunc which rotates the ships 90 degrees and back when clicked again
+let angle = 0 
 function flipFunc() {
     const shipOptions = Array.from(shipsContainer.children)
     if (angle === 0) {
@@ -11,5 +16,26 @@ function flipFunc() {
     }
         shipOptions.forEach(shipOption => shipOption.style.transform = `rotate(${angle}deg)`)
 }
+
+const width = 20
+function createGameBoard(user) {
+    const gameBoard = document.createElement('div')
+    gameBoard.classList.add('game-board')
+    gameBoard.style.backgroundColor = 'black'
+    gameBoard.id = user
+
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoard.append(block)
+    }
+
+    gameArea.append(gameBoard)
+}
+
+
+createGameBoard('player')
+createGameBoard('computer')
 
 flipButton.addEventListener('click', flipFunc)
