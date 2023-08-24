23eb4f3b85bb423df9026b06181bc386d8a39c7c
richie nolan <richienolan23@gmail.com>
2023-08-24 19:07:05 +0000

    add function to flip ships 90 degrees

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-function startGame()
+const shipsContainer = document.querySelector('#ships-container')
+const flipButton = document.querySelector('#flip-button')
+let angle = 0 
 
-function placeShips()
+function flipFunc() {
+    const shipOptions = Array.from(shipsContainer.children)
+    if (angle === 0) {
+        angle = 90
+    } else {
+        angle = 0
+    }
+        shipOptions.forEach(shipOption => shipOption.style.transform = `rotate(${angle}deg)`)
+}
 
+flipButton.addEventListener('click', flipFunc)
